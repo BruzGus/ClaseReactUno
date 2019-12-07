869068f64d238065774d7f06f40e93b12bb5db3b
Bryan Augusto Cruz Castro <bryan.cc.652@gmail.com>
2019-12-07 12:35:56 -0700

    incorporacion nuevos archivos para el manejo de modulos

---
+++ app1.js
@@ -0,0 +1,6 @@
+/*manejo de importanciones*/
+export const mascota={
+    nick :'forrest',
+    raza :'caniche',
+    age : 15
+};
---
+++ app2.js
@@ -0,0 +1,4 @@
+import{ mascota } from "./app1.js";
+
+
+console.log(mascota.nick);
