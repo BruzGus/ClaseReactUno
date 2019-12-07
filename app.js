ac84aca6f18835bdb73f88ae2c365b162ac7cfc6
Bryan Augusto Cruz Castro <bryan.cc.652@gmail.com>
2019-12-07 11:53:39 -0700

    Manejo de Objetos en la Clase.

--- app.js
+++ app.js
@@ -11,3 +11,26 @@ if (numero < 5) {
 var array =[1,2,3,4];
 
 array.map( numero => console.log(numero))
+ /*MANEJO DE FUCIONES*/
+
+
+ function suma(n1, n2 ){
+     return n1 + n2;
+ };
+
+
+ console.log (suma (1,5));
+
+
+ var restar = function ( n1 , n2){ return n1 -n2}
+
+ console.log(restar);
+
+ /*OBJETOS EN JAVA SCRIPT*/
+ var persona ={
+    name : 'Bryan Agusto',
+    lastName : 'Cruz Castro' ,
+    age :32
+}
+
+console.log(persona)
